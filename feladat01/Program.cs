5868619b9179193f96bcf4325a6ef0dc5f79f846
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-09-06 13:05:36 +0200

    Első feladat kész

--- feladat01/Program.cs
+++ feladat01/Program.cs
@@ -8,8 +8,23 @@ namespace feladat01
 {
     class Program
     {
+        static int[] het52 = new int[5];
         static void Main(string[] args)
         {
+            ElsoFeladat();
+
+            Console.ReadKey();
+        }
+
+        private static void ElsoFeladat()
+        {
+            Console.WriteLine("1. feladat: 52 hét számai:");
+
+            for (int i = 0; i < het52.Length; i++)
+            {
+                Console.WriteLine($"Kérem írja be az {i + 1}. számot: ");
+                het52[i] =  Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
